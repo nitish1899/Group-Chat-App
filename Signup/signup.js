08cf3a26b99533a22644cd3c88c0c9af8a759fb7
nitish1899 <nkword1899@gmail.com>
2023-02-20 19:57:15 +0530

    Sign Up frontend page

---
+++ Signup/signup.js
@@ -0,0 +1,27 @@
+async function Signup(event){
+    try{
+        event.preventDefault();
+        const userDetails = {
+            name: event.target.name.value,
+            email: event.target.email.value,
+            password: event.target.password.value,
+            phNo: event.target.PhoneNumber.value
+        }
+    
+        document.getElementById('name').value = "";
+        document.getElementById('email').value = "";
+        document.getElementById('password').value = "";
+        document.getElementById('PhoneNumber').value = "";
+    
+        const response = await axios.post('http://localhost:3000/user/signup',userDetails);
+        if(response.status === 201){
+            alert(response.data.message);
+           // window.location.href='../Login/login.html';
+        }
+        else {
+            throw new Error(" Failed to Sign Up");
+        }
+    } catch(err) {
+       document.body.innerHTML +=`<div style="color:red;">${err}</div>`;
+    }
+}
